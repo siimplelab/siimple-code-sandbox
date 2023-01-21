e4d258819f7ccbd263692abe05a64023788bf092
choi <choi@siimplelab.com>
2023-01-21 22:48:46 +0900

    Javascript Object

--- js-sandbox/js-object/main.js
+++ js-sandbox/js-object/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Reference URL: https://www.youtube.com/watch?v=1Lbr29tzAA8&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=7
+
 // * Ojbects
 // one of the Javascript's data types
 // a collection of related data and/or functionality.
@@ -58,5 +60,51 @@ function Person(name, age) {
 console.log('name' in ellie);
 console.log('age' in ellie);
 console.log('random' in ellie);
-console.log(ellie.radnom);
+console.log(ellie.radnom); // undeifned
 
+// * 6. for..in vs for..of'
+// for (key in obj)
+// 오브젝트를 대상으로 한 속성 반복문
+console.clear();
+for (const key in ellie) {
+  console.log(key);
+}
+
+// for (value of iterable)
+// 배열을 대상으로 한 요소 반복문
+const array = [1, 2, 3, 4, 5];
+// for(let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+for (const value of array) {
+  console.log(value);
+}
+
+// * 7. Fun cloning
+// Object.assign(dest, [obj1, obj2, obj3...])
+const user = { name: 'ellie', age: '20' };
+const user2 = user;
+user2.name = 'coder';
+console.log(user); // 'coder'
+
+// * old way of assigning properties to a new Object
+const user3 = {};
+for (const key in user) {
+  user3[key] = user[key];
+  console.log(user[key]);
+}
+
+console.clear();
+
+// * new way of assigning properties to a new Object
+const user4 = {};
+Object.assign(user4, user3);
+// 새로운 메소드를 쓰거나 할 때는 리턴값이 무엇인지 확인하는 것이 좋음
+console.log(user4);
+
+// * another example
+const fruit1 = { color: 'red' };
+const fruit2 = { color: 'blue', size: 'big' };
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed.color);
+console.log(mixed.size);
