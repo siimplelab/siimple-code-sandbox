34d3e98be822621f88e12bfd719485d94abe4fb2
siimplelab <info@siimplelab.com>
2023-01-24 00:46:49 +0900

    Javascript APIs study additional

--- js-sandbox/js-array-apis/main.js
+++ js-sandbox/js-array-apis/main.js
@@ -46,7 +46,7 @@ const students = [
   new Student('A', 29, true, 45),
   new Student('B', 28, false, 80),
   new Student('C', 30, true, 90),
-  new Student('D', 40, false, 66),
+  new Student('D', 40, false, 90),
   new Student('E', 18, true, 88)
 ]
 
@@ -59,7 +59,7 @@ const students = [
     //  return student;
     // }
     return student.score === 90;
-    // console.log(student.score === 90);
+    // return true;
   });
   console.log(result);
 }
@@ -67,12 +67,71 @@ const students = [
 // * Q6. make an array of enrolled students
 {
   // * A6. FILTER()
-  // const result = students.filter((student) => student.enrolled = true);
-  // console.log(result);
+  const result = students.filter((student) => {
+    // student.enrolled === true;
+    return true;
+  });
+  console.log(result);
 }
 
 // * Q7. make an array containing only the students' scores
+// result should be: [45, 80, 90, 66, 88]
 {
-  // * A7.
-  // const result = students.
+  // * A7. MAP()
+  const result = students.map((student) => {
+    // student.score
+    return student.score;
+  });
+  console.log(result);
+}
+
+// * Q8. check if there is a student with the score lower than 50
+{
+  // * A8. SOME(), EVERY()
+  let result = students.some((student) => {
+    return student.score < 50;
+  });
+  console.log(result);
+
+  result = students.every((student) => student.score < 50);
+  console.log (result);
+}
+
+// * Q9. compute students' average score
+{
+  // * A9. REDUCE()
+  const result = students.reduce((prev, curr) => {
+    // console.log(prev);
+    // console.log(curr.score);
+    return (prev + curr.score / students.length);
+    // 리턴값들이 prev에 순차적으로 할당됨
+  }, 0);
+  console.log(result);
+}
+
+// * Q10. make a string containing all the scores
+// result should be: '45, 80, 90, 60, 88'
+{
+  const result = students.map((student) => {
+    return student.score;
+  })
+  .filter((score) => score >= 50)
+  .join(', ');
+  console.log(result);
+}
+
+// * Q11. do Q1 sorted in ascending order
+// result should be: '45, 66, 80, 88, 90'
+{
+  // * A11. SORT()
+  console.clear();
+  const result = students.map((student) => {
+    return student.score;
+  }).sort((a,b) => {
+    // return a - b;
+    return b - a;
+    // console.log(b);
+    // console.log(b - a);
+  });
+  console.log(result);
 }
